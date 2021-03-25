4fe93dc6449bb05a8449f241afc3432d30d940c2
rohit20001221 <rohit20001221@gmail.com>
2021-03-25 20:56:24 +0530

    DFS  template revisited

--- stacks_que/dfs_template.py
+++ stacks_que/dfs_template.py
@@ -1,11 +1,17 @@
-def DFS(cur, target, visited):
-    if cur == target:
-        return True
-    
-    for next_ in cur.neighbors():
-        if next_ not in visited:
-            visited.append(next_)
-            return DFS(next_, target, visited)
-    
-    return False
-    
+def DFS_Recursive(v,visited):
+    visited[v] = True
+
+    for neighbour in v.neighbours():
+        if not visited[neighbour]:
+            DFS_Recursive(v, visited)
+
+def DSF(v, visited):
+    stack = []
+    stach.append(v)
+
+    while len(stack) > 0:
+        v_ = stack.pop()
+        if not visited[v_]:
+            visited[v_] = True
+            for neighbour in v_.neighbours():
+                stack.push(neighbour)
